cd132204776172a0aa0d92a56d9895913a47e15b
Top Code <162437900+Coderad32@users.noreply.github.com>
2026-08-14 18:55:51 -0600

    Optimize drop logic in SIGIL_MATRIX.sh
    Refactor drop positions and speeds for visual effects.

--- SIGIL_MATRIX.sh
+++ SIGIL_MATRIX.sh
@@ -22,17 +22,16 @@ chars=(
     '%)' '%!' '%@' '%#' '%$' '%%' '%^' '%&' '%*' '%(' # 59
     '%)' '^!' '^@' '^#' '^$' '^%' '^^' '^&' '^*' '^(' # 69
     '&)' '&!' '&@' '&#' '&$' '&%' '&^' '&&' '&*' '&(' # 79
-    '*)' '*!' '*@' '*#' '*$' '*%' '*^' '*&' '**' '*(' # 89  
+    '*)' '*!' '*@' '*#' '*$' '*%' '*^' '*&' '**' '*(' # 89
     '()' '(!' '(@' '(#' '($' '(%' '(^' '(&' '(*' '((' # 99
     '!))' # 100
 )
 num_chars=${#chars[@]}
 
-# Initialize drop positions, lengths, and speeds for each column across the full width
+# Initialize drop positions and speeds for each column across the full width
 for ((i=0; i<cols; i++)); do
-    y_pos[i]=$(( (RANDOM % rows) - rows ))
-    length[i]=$((RANDOM % 8 + 6))
-    speed[i]=$((RANDOM % 2 + 1))
+    y_pos[i]=$((RANDOM % rows))
+    speed[i]=$((RANDOM % 3 + 1))
 done
 
 # Main loop
@@ -43,44 +42,31 @@ while true; do
 
     for ((x=0; x<cols; x++)); do
         col=$((x + 1))
-        head=${y_pos[x]}
-        len=${length[x]}
 
-        # Draw the falling trail for this column
-        for ((j=0; j<len; j++)); do
-            curr_y=$((head - j))
-            
-            if (( curr_y > 0 && curr_y <= rows )); then
-                char_idx=$((RANDOM % num_chars))
-                char="${chars[$char_idx]}"
-                
-                if (( j == 0 )); then
-                    # Bright white leading head
-                    printf "\033[%d;%dH\033[1;37m%s" "$curr_y" "$col" "$char"
-                elif (( j < 3 )); then
-                    # Bright green near the head
-                    printf "\033[%d;%dH\033[1;32m%s" "$curr_y" "$col" "$char"
-                else
-                    # Dim green fading trail using original sigils
-                    printf "\033[%d;%dH\033[0;32m%s" "$curr_y" "$col" "$char"
-                fi
-            fi
-        done
+        # Print bright head
+        char_idx=$((RANDOM % num_chars))
+        char="${chars[$char_idx]}"
+        printf "\033[%d;%dH\033[1;37m%s" "${y_pos[x]}" "$col" "$char"
 
-        # Clear the space directly behind the tail
-        tail_y=$((head - len))
-        if (( tail_y > 0 && tail_y <= rows )); then
-            printf "\033[%d;%dH " "$tail_y" "$col"
+        # Print trail
+        trail_y=$((y_pos[x] - 4))
+        if (( trail_y > 0 && trail_y <= rows )); then
+            trail_char_idx=$((RANDOM % num_chars))
+            trail_char="${chars[$trail_char_idx]}"
+            printf "\033[%d;%dH\033[0;32m%s" "$trail_y" "$col" "$trail_char"
         fi
 
-        # Advance position based on individual speed
+        # Clear tail
+        end_y=$((y_pos[x] - 12))
+        if (( end_y > 0 && end_y <= rows )); then
+            printf "\033[%d;%dH " "$end_y" "$col"
+        fi
+
+        # Advance position
         y_pos[x]=$((y_pos[x] + speed[x]))
-        
-        # Reset drop once it falls past the bottom
-        if (( y_pos[x] - len > rows )); then
+        if (( y_pos[x] > rows + 12 )); then
             y_pos[x]=$(( (RANDOM % 5) - 5 ))
-            length[x]=$((RANDOM % 8 + 6))
-            speed[x]=$((RANDOM % 2 + 1))
+            speed[x]=$((RANDOM % 3 + 1))
         fi
     done
 
